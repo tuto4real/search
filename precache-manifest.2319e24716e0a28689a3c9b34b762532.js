7546901822875986f72fb10f744a650a8e8d7816
Thawindu Angesh Tuto <tuto4real@icloud.com>
2019-01-23 11:38:38 +0530

    Add files via upload

---
+++ precache-manifest.2319e24716e0a28689a3c9b34b762532.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a7ab0b2a2a266bdf8d84",
+    "url": "/static/js/main.a7ab0b2a.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "a7ab0b2a2a266bdf8d84",
+    "url": "/static/css/main.997a7c98.chunk.css"
+  },
+  {
+    "revision": "50a437d01b843994855b87ad3e579dbc",
+    "url": "/index.html"
+  }
+];
